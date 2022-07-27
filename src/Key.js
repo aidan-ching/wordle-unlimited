b6d4f1570a9fb5a75035a2fe9fe7d55d467ddc2d
Aidan Ching <achin053@ucr.edu>
2022-07-26 22:51:37 -0700

    removed a bug fixing console log message that is not needed anymore

--- src/Key.js
+++ src/Key.js
@@ -4,7 +4,6 @@ const Key = ( {text, keyPress, discovered, yellowLetters, greenLetters} ) => {
   return (
     <div className='key' onClick={(e) =>{
         keyPress(e);
-        console.log(discovered.includes(text));
     }}
      style={{backgroundColor: greenLetters.includes(text) ? "green" : yellowLetters.includes(text) ? "#9e970b" : discovered.includes(text) ? '#2b2b2b' : 'grey'}}>
         {text}
